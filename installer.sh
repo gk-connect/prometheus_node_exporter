a1f1600d924f9de500537ec140ee7d5a6d8146af
gopukrishna <gopukrishnan222@gmail.com>
2021-05-27 12:24:26 +0530

    fixed file name issue

--- installer.sh
+++ installer.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 sudo useradd --no-create-home node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
-tar xzf node_exporter-1.1.0.linux-amd64.tar.gz
-sudo rm node_exporter-1.1.0.linux-amd64.tar.gz
-sudo mv node_exporter-1.1.0.linux-amd64  /usr/local/bin/node_exporter
+tar xzf node_exporter-1.1.2.linux-amd64.tar.gz
+sudo rm node_exporter-1.1.2.linux-amd64.tar.gz
+sudo mv node_exporter-1.1.2.linux-amd64  /usr/local/bin/node_exporter
 sudo cp node-exporter.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
